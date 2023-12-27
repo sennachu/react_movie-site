19c50a3816316e131f7b17e71c4a76a2e07d6a83
Sena Bezirkan <90784360+sennachu@users.noreply.github.com>
2023-12-28 02:41:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Homepage from './components/Homepage'
+import ProtectedPage from './ProtectedPage'
 function App() {
   return(
     <>
-      <Homepage/>
+      <ProtectedPage/>
     </>
   )
 }
