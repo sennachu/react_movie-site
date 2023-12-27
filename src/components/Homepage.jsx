ee7cf7a031d89023cf965640445b113ed8516191
Sena Bezirkan <90784360+sennachu@users.noreply.github.com>
2023-12-28 02:17:26 +0300

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button} from "@mui/material";
+
 import '../style/home.css'
 
 function Homepage() {
@@ -7,9 +7,9 @@ function Homepage() {
     <>
       <div className='header'>222016746 Sena Bezirkan Web Geliştirme Teknikleri Vize Projesine Hoş Geldiniz</div>
       <div className='inner'>
-        <Button color="warning" variant="contained" >
+
           <a id='sign-in' href='/Ana_Sayfa'>Giriş Yap</a>
-        </Button>
+
       </div>
 
     </>
